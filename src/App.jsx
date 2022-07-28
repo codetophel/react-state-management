4da8326b4d96be91627967ce83a6ed3fd129ad49
codetophel <nicodemusakande@gmail.com>
2022-07-28 03:12:27 +0100

    modified:   src/App.jsx 	modified:   src/Cart.jsx 	modified:   src/Detail.jsx 	new file:   src/services/useFetchAll.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
@@ -8,6 +8,22 @@ import Detail from './Detail';
 import Cart from './Cart';
 
 export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    setCart((items) => {
+      const itemInCart = items.find((i) => i.sku === sku);
+      if (itemInCart) {
+        //if there is the item in the cart already, map through the existing items array then return a copy of the array with the matching item replaced
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        //return a new array with the new item included
+        return [...items, { id: id, sku, quantity: 1 }];
+      }
+    });
+  }
   return (
     <>
       <div className='content'>
@@ -19,8 +35,11 @@ export default function App() {
               element={<h1> Welcome to Carved Rock Fitness</h1>}
             />
             <Route path='/:category' element={<Products />} />
-            <Route path='/:category/:id' element={<Detail />} />
-            <Route path='/cart' element={<Cart />} />
+            <Route
+              path='/:category/:id'
+              element={<Detail addToCart={addToCart} />}
+            />
+            <Route path='/cart' element={<Cart cart={cart} />} />
           </Routes>
         </main>
       </div>

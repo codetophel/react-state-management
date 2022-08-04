10def295da23815f4c142b90944431098df45afa
codetophel <nicodemusakande@gmail.com>
2022-08-04 12:28:22 +0100

    new file:   src/DetailRef.jsx

---
+++ src/DetailRef.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from './services/useFetch';
+import Spinner from './Spinner';
+import PageNotFound from './PageNotFound';
+import { useNavigate } from 'react-router-dom';
+
+export default function Detail(props) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const skuRef = useRef();
+
+  const { data: product, error, loading } = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+
+  // Display these products details
+  return (
+    <div id='detail'>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id='price'>${product.price}</p>
+      <select id='size' ref={skuRef}>
+        <option value=''>What Size</option>
+        {product.skus.map((s) => (
+          <option key={s.sku} value={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
+      <p>
+        <button
+          className='btn btn-primary'
+          onClick={() => {
+            const sku = skuRef.current.value;
+            if (!sku) return alert('Select Size');
+            props.addToCart(id, sku);
+            navigate('/cart');
+          }}
+        >
+          Add to Cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
+}
